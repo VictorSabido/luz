c2e56e2e12795863e7b1697cb1491d913bbf0235
VictorSabido <vsabidorubio@gmail.com>
2023-04-17 18:05:41 +0200

    Added Guzzle package and key Ree service

---
+++ app/Services/Ree.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class Ree
+{
+    protected $apiKey;
+    protected $locale = 'es';
+    protected $url = 'https://api.esios.ree.es/archives/70/download_json';
+
+    public function __construct()
+    {
+        $this->apiKey = $this->getApiKey();
+    }
+
+    protected function getApiKey()
+    {
+        return config('api.ree.api_key');
+    }
+
+    public function getPriceByDate($date)
+    {
+        Log::info('API CALLED: Getting price for ' . $date . ' from REE');
+
+        return Http::withHeaders([
+            'x-api-key' => $this->apiKey,
+        ])->get($this->url, [
+            'locale' => $this->locale,
+            'date' => $date,
+        ])->json();
+    }
+
+
+}
---
+++ config/api.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ree' => [
+        'api_key' => env('REE_API_KEY'),
+    ],
+];
